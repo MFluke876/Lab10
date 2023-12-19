5a02f83d268c214531da74b9918015137f2283a9
Fluke <dr.catth@gmail.com>
2023-12-19 21:32:43 +0700

    lab10_3 completed

--- lab10_3.cpp
+++ lab10_3.cpp
@@ -1,3 +1,29 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+    ifstream score;
+    score.open("score.txt");
+
+    string num;
+    int count = 0;
+    double total = 0, n = 0, mean, sd;
+    
+    while(getline(score,num)){
+        count++;
+        total = total + stod(num);
+        mean = total/count;
+        n = n + pow(stod(num),2);
+        sd = sqrt((n/count) - pow(mean,2));
+    }
+
+    cout << "Number of data = " << count << '\n';
+    cout << setprecision(3);
+    cout << "Mean = " << mean << '\n';
+    cout << "Standard deviation = " << sd << '\n';
+}
